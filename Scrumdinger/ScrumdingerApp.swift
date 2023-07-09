85566fab2775cffd34446323fc31e2f817d2ca5a
Alexandre Lima <alexmelolima@yahoo.com.br>
2023-07-08 23:11:31 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alexandre Lima on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
